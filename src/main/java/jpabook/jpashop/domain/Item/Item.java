241e96d0fb0d6fb2e7cf25c2104f1709df99ba1e
junyoung-choe <wjdghkswnsdu@naver.com>
2024-01-13 21:53:14 +0900

    feat: JPA, RDB, 관계 설정

---
+++ src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain.Item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private Integer price;
+    private Integer stocckQuantity;
+
+    @OneToMany(mappedBy = "items")
+    private List<Category> categoties = new ArrayList<Category>();
+}
--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,13 +1,10 @@
 package jpabook.jpashop;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,21 +12,21 @@ public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Test
-    @Transactional
-    @Rollback(false)
-    public void testMember() throws Exception {
-        //given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        //that
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장 하나의 트랜잭션(영속성 컨텍스트 1차 캐시) 에서 올려놓은걸 가져와서 !
-    }
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    public void testMember() throws Exception {
+//        //given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        //when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+//
+//        //that
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장 하나의 트랜잭션(영속성 컨텍스트 1차 캐시) 에서 올려놓은걸 가져와서 !
+//    }
 }
